aba58ecc913e78e26155ea9a6b427b8fab5cb490
henriquedevgmz <henriquedev.gmz@gmail.com>
2026-05-06 10:18:58 -0400

    🔧 configura TypeScript para usar tsc watch

--- modulo-01/hello-world/app.d.ts
+++ modulo-01/hello-world/app.d.ts
@@ -1,2 +1,12 @@
+/**
+ *
+ * @module HelloWorld
+ * @description
+ * Demmonstra conceito básico de TypeScript.
+ *
+ * @author Henrique Giacumuzzi
+ * @date 06-05-2026
+ *
+ */
 export {};
 //# sourceMappingURL=app.d.ts.map
--- modulo-01/hello-world/app.js
+++ modulo-01/hello-world/app.js
@@ -1,4 +1,14 @@
 "use strict";
+/**
+ *
+ * @module HelloWorld
+ * @description
+ * Demmonstra conceito básico de TypeScript.
+ *
+ * @author Henrique Giacumuzzi
+ * @date 06-05-2026
+ *
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 let mensagem = "Hello World, Henrique!";
 console.log(mensagem);
