8c7977e0683cc575b7858a9b2d520ac6256e4884
henriquedevgmz <henriquedev.gmz@gmail.com>
2026-05-06 09:45:50 -0400

    💡 adiciona comentarios ao modulo-01 .

--- modulo-01/hello-world/app.ts
+++ modulo-01/hello-world/app.ts
@@ -1,2 +1,13 @@
+/**
+ *
+ * @module HelloWorld
+ * @description
+ * Demmonstra conceito básico de TypeScript.
+ *
+ * @author Henrique Giacumuzzi
+ * @date 06-05-2026
+ *
+ */
+
 let mensagem: string = "Hello World, Henrique!";
 console.log(mensagem);
